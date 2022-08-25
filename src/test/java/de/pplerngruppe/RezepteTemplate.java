263d4413682e003a49822b86ed7dfcb4cf51e15c
Sina Shahavi <63404069+shahavi84@users.noreply.github.com>
2022-08-25 12:50:02 +0200

    In RezeptTemplate Klasse Methoden mit einer Zutat, mehrerer Zutaten ohne Zubereitung, und Rezepte mit Zutaten und Zubereitung erstellt

--- src/test/java/de/pplerngruppe/RezepteTemplate.java
+++ src/test/java/de/pplerngruppe/RezepteTemplate.java
@@ -5,7 +5,24 @@ public class RezepteTemplate {
     public static Rezept leeresRezept(){
         return new Rezept("leeres Rezept");
     }
-    public static Rezept KaeseKuchenRezept(){
+
+    public static Rezept eineZutat(){
+        Zutat zutat = new Zutat("Wasser",new Menge(200));
+        Rezept rezept = new Rezept("WasserRezept");
+        rezept.add(zutat);
+        return rezept;
+    }
+
+    public static Rezept ohneZubereitungMitMehrereZutaten(){
+        Zutat zutat1 = new Zutat("Käse",new Menge(400));
+        Zutat zutat2 = new Zutat("Kuchen",new Menge(100));
+        Rezept rezept = new Rezept("Käsekuchen");
+        rezept.add(zutat1);
+        rezept.add(zutat2);
+        return rezept;
+    }
+
+    public static Rezept zubereitungMitMehrereZutaten(){
         Zutat zutat1 = new Zutat("Käse",new Menge(500));
         Zutat zutat2 = new Zutat("Kuchen",new Menge(300));
         Rezept rezept = new Rezept("Käsekuchen");
@@ -14,4 +31,5 @@ public class RezepteTemplate {
         rezept.setZubereitung("Mischen");
         return rezept;
     }
+
 }
